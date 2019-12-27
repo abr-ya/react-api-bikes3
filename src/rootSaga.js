a612317a6853321446da6e567351ee53b0de633d
gamer05 <gamer05@list.ru>
2019-12-27 11:36:20 +0300

    Обновлен (добавлен) serialize-javascript.

--- src/rootSaga.js
+++ src/rootSaga.js
@@ -5,6 +5,4 @@ import * as sagas from './sagas';
 export default function* rootSaga() {
 	yield takeLatest(actionTypes.INIT_APP, sagas.initAppSaga);	
 	yield takeLatest(actionTypes.DISPLAY_NET, sagas.displayNetSaga);
-	//yield takeLatest(authActionTypes.VERIFY_TOKEN, verifyTokenSaga);
-	//yield takeLatest(authActionTypes.REFRESH_TOKEN, refreshTokenSaga);
 }
